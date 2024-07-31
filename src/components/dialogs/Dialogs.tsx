c10c822cb90fba97c726ed68c4b38eae3b0e3ca8
Denis <denis140292@gmail.com>
2024-07-31 19:52:08 +0300

    feat: Add Dialogs items.

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -8,7 +8,32 @@ interface IDialogs {
 const Dialogs: FC<IDialogs> = ({}) => {
   return (
     <div className={s.wrapper}>
-      Dialogs
+      <div className={s.dialogItems}>
+        <div className={s.item}>
+          Name 1
+        </div>
+        <div className={s.item}>
+          Name 2
+        </div>
+        <div className={s.item}>
+          Name 3
+        </div>
+        <div className={s.item}>
+          Name 4
+        </div>
+      </div>
+
+      <div className={s.messages}>
+        <div className={s.message}>
+          Message 1
+        </div>
+        <div className={s.message}>
+          Message 2
+        </div>
+        <div className={s.message}>
+          Message 3
+        </div>
+      </div>
     </div>
   );
 };
